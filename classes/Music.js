67d1542c3ae535cb82c4ada10e19d7b6d2d05ed4
David Kwon <david.kwon@clover.com>
2024-10-28 15:38:41 -0600

    Implement Music class in Music.js

--- classes/Music.js
+++ classes/Music.js
@@ -1,6 +1,26 @@
 // import the Media class:
+const Media = require("./Media");
 
 // create your Music class:
+class Music extends Media {
+    constructor(title, year, genre, artist, length) {
+        super(title, year, genre);
+        this.artist = artist;
+        this.length = length;
+    }
+
+    summary() {
+        return `Title: ${this.title}, Artist: ${this.artist}, Year: ${this.year}, Genre: ${this.genre}, Length: ${this.length} seconds`
+    }
+
+    static shortestAlbum(tracks) {
+        const trackLengths = tracks.map((track) => {
+            return track.length;
+        })
+        const targetIndex = trackLengths.indexOf(Math.min(...trackLengths));
+        return tracks[targetIndex];
+    }
+}
 
 // don't change below
 module.exports = Music;
