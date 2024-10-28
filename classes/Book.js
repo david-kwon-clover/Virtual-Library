024185381c5c75ed390e8ff6eef882f09cf8e1d5
David Kwon <david.kwon@clover.com>
2024-10-28 15:28:39 -0600

    Implement Book class in Book.js

--- classes/Book.js
+++ classes/Book.js
@@ -1,6 +1,27 @@
 // import the Media class:
+const Media = require("./Media");
 
 // create your Book class:
+class Book extends Media {
+    constructor(title, year, genre, author, numPages, rating) {
+        super(title, year, genre);
+        this.author = author;
+        this.numPages = numPages;
+        this.rating = rating;
+    }
+
+    summary() {
+        return `Title: ${this.title}, Author: ${this.author}, Year: ${this.year}, Page Count: ${this.numPages}, Genre: ${this.genre}, Rating: ${this.rating}`;
+    }
+
+    static highestRating(books) {
+        const ratings = books.map((book) => {
+            return book.rating;
+        })
+        const targetIndex = ratings.indexOf(Math.max(...ratings));
+        return books[targetIndex];
+    }
+}
 
 // don't change below
 module.exports = Book;
