a93d3faa2f40068136af9d188256754e5337c7a6
David Kwon <david.kwon@clover.com>
2024-10-29 08:01:32 -0600

    Implement Podcast class in Podcast.js

--- classes/Podcast.test.js
+++ classes/Podcast.test.js
@@ -1,24 +1,38 @@
 const Music = require("./Music");
 const Podcast = require("./Podcast");
-const { describe, test, expect, beforeEach } = require("@jest/globals"); 
+const { describe, test, expect, beforeEach } = require("@jest/globals");
 
-describe('Podcast tests' , () => {
-    beforeEach(() => {
-        const testPodcast = new Podcast("The Basement Yard", 2015, "Comedy", "Basement Boys", 361, "Joe & Frank", "Frank's Big Fight", 72, "N/A");
-    })
+describe("Podcast tests", () => {
+  let testPodcast;
+  beforeEach(() => {
+    testPodcast = new Podcast(
+      "The Basement Yard",
+      2015,
+      "Comedy",
+      "Basement Boys",
+      361,
+      "Joe & Frank",
+      "Frank's Big Fight",
+      72,
+      "N/A"
+    );
+  });
 
-    test('Can create a new Podcast instance that is child of Music', () => {
-        expect(testPodcast).toBeInstanceOf(Music);
-    })
+  test("Can create a new Podcast instance that is child of Music", () => {
+    expect(testPodcast).toBeInstanceOf(Podcast);
+    expect(testPodcast).toBeInstanceOf(Music);
+  });
 
-    test('Podcast has properties of host, episodeName, episodeNumber, and guests', () => {
-        expect(testPodcast).toHaveProperty("host", "Joe & Frank");
-        expect(testPodcast).toHaveProperty("episodeName", "Frank's Big Fight");
-        expect(testPodcast).toHaveProperty("episodeNumber", 72);
-        expect(testPodcast).toHaveProperty("guests", "N/A");
-    })
+  test("Podcast has properties of host, episodeName, episodeNumber, and guests", () => {
+    expect(testPodcast).toHaveProperty("host", "Joe & Frank");
+    expect(testPodcast).toHaveProperty("episodeName", "Frank's Big Fight");
+    expect(testPodcast).toHaveProperty("episodeNumber", 72);
+    expect(testPodcast).toHaveProperty("guests", "N/A");
+  });
 
-    test('Podcast summary method returns the correct summary', () => {
-        expect(testPodcast.listen()).toBe("The Basement Yard - Episode: Frank's Big Fight. Hosted by Basement Boys and featuring guests N/A. Length: 361 seconds.");
-    })
-})
+  test("Podcast summary method returns the correct summary", () => {
+    expect(testPodcast.listen()).toBe(
+      "The Basement Yard - Episode: Frank's Big Fight. Hosted by Joe & Frank and featuring guests N/A. Length: 361 seconds."
+    );
+  });
+});
