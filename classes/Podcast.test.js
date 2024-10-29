13d446769fac1c3315eb5f18eb34e475fe14e201
David Kwon <david.kwon@clover.com>
2024-10-29 07:52:25 -0600

    Write tests for TDD of Podcast class

---
+++ classes/Podcast.test.js
@@ -0,0 +1,24 @@
+const Music = require("./Music");
+const Podcast = require("./Podcast");
+const { describe, test, expect, beforeEach } = require("@jest/globals"); 
+
+describe('Podcast tests' , () => {
+    beforeEach(() => {
+        const testPodcast = new Podcast("The Basement Yard", 2015, "Comedy", "Basement Boys", 361, "Joe & Frank", "Frank's Big Fight", 72, "N/A");
+    })
+
+    test('Can create a new Podcast instance that is child of Music', () => {
+        expect(testPodcast).toBeInstanceOf(Music);
+    })
+
+    test('Podcast has properties of host, episodeName, episodeNumber, and guests', () => {
+        expect(testPodcast).toHaveProperty("host", "Joe & Frank");
+        expect(testPodcast).toHaveProperty("episodeName", "Frank's Big Fight");
+        expect(testPodcast).toHaveProperty("episodeNumber", 72);
+        expect(testPodcast).toHaveProperty("guests", "N/A");
+    })
+
+    test('Podcast summary method returns the correct summary', () => {
+        expect(testPodcast.listen()).toBe("The Basement Yard - Episode: Frank's Big Fight. Hosted by Basement Boys and featuring guests N/A. Length: 361 seconds.");
+    })
+})
