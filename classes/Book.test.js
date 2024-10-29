ad3148b7fb322c51e015703955ba897fcc6187d7
David Kwon <david.kwon@clover.com>
2024-10-29 08:33:53 -0600

    Implement static calculateAverageRating method in Book and Movie classes and static ALL_MEDIA property in Media class

--- classes/Book.test.js
+++ classes/Book.test.js
@@ -1,73 +1,101 @@
-const Book = require('./Book')
-const Media = require('./Media')
+const Book = require("./Book");
+const Media = require("./Media");
 
-describe('Book Tests', () => {
-  test('Can create a new Book instance that is child of Media', () => {
+describe("Book Tests", () => {
+  test("Can create a new Book instance that is child of Media", () => {
     const book1 = new Book(
-      'To Kill a Mockingbird',
+      "To Kill a Mockingbird",
       1960,
-      'Fiction',
-      'Harper Lee',
+      "Fiction",
+      "Harper Lee",
       281,
       4.4
-    )
-    expect(book1 instanceof Book).toBe(true)
-    expect(book1 instanceof Media).toBe(true)
-  })
+    );
+    expect(book1 instanceof Book).toBe(true);
+    expect(book1 instanceof Media).toBe(true);
+  });
 
-  test('Book has author, numPages, and rating property', () => {
+  test("Book has author, numPages, and rating property", () => {
     const book1 = new Book(
-      'To Kill a Mockingbird',
+      "To Kill a Mockingbird",
       1960,
-      'Fiction',
-      'Harper Lee',
+      "Fiction",
+      "Harper Lee",
       281,
       4.4
-    )
-    expect(book1.author).toBe('Harper Lee')
-    expect(book1.numPages).toBe(281)
-    expect(book1.rating).toBe(4.4)
-  })
+    );
+    expect(book1.author).toBe("Harper Lee");
+    expect(book1.numPages).toBe(281);
+    expect(book1.rating).toBe(4.4);
+  });
 
-  test('Book has summary method different from the Media summary method', () => {
+  test("Book has summary method different from the Media summary method", () => {
     const book1 = new Book(
-      'To Kill a Mockingbird',
+      "To Kill a Mockingbird",
       1960,
-      'Fiction',
-      'Harper Lee',
+      "Fiction",
+      "Harper Lee",
       281,
       4.4
-    )
+    );
     expect(book1.summary()).toBe(
-      'Title: To Kill a Mockingbird, Author: Harper Lee, Year: 1960, Page Count: 281, Genre: Fiction, Rating: 4.4'
-    )
-  })
+      "Title: To Kill a Mockingbird, Author: Harper Lee, Year: 1960, Page Count: 281, Genre: Fiction, Rating: 4.4"
+    );
+  });
 
-  test('Book has static method highestRating that returns the highest rated book', () => {
+  test("Book has static method highestRating that returns the highest rated book", () => {
     const book1 = new Book(
-      'To Kill a Mockingbird',
+      "To Kill a Mockingbird",
       1960,
-      'Fiction',
-      'Harper Lee',
+      "Fiction",
+      "Harper Lee",
       281,
       4.4
-    )
+    );
     const book2 = new Book(
-      'The Bluest Eye',
+      "The Bluest Eye",
       1970,
-      'Fiction',
-      'Toni Morrison',
+      "Fiction",
+      "Toni Morrison",
       206,
       4.6
-    )
+    );
     const book3 = new Book(
-      'My first novel',
+      "My first novel",
       2020,
-      'Fiction',
-      'Biblophile',
+      "Fiction",
+      "Biblophile",
       121,
       2.2
-    )
-    expect(Book.highestRating([book1, book2, book3])).toEqual(book2)
-  })
-})
+    );
+    expect(Book.highestRating([book1, book2, book3])).toEqual(book2);
+  });
+
+  test("Book static calculateAverageRating method returns the correct average ratings given an array of books", () => {
+    const book1 = new Book(
+      "To Kill a Mockingbird",
+      1960,
+      "Fiction",
+      "Harper Lee",
+      281,
+      4.4
+    );
+    const book2 = new Book(
+      "The Bluest Eye",
+      1970,
+      "Fiction",
+      "Toni Morrison",
+      206,
+      4.6
+    );
+    const book3 = new Book(
+      "My first novel",
+      2020,
+      "Fiction",
+      "Biblophile",
+      121,
+      2.7
+    );
+    expect(Book.calculateAverageRating([book1, book2, book3])).toBe(3.9);
+  });
+});
